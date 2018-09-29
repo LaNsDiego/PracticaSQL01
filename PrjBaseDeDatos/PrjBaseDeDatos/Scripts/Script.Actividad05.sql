f2b0c92183c63382b48366330d39a54337a2a84f
Juan Rodriguez <nyxjuan@hotmail.com>
2018-09-29 01:22:52 -0500

    Actividad 05 y 06

---
+++ PrjBaseDeDatos/PrjBaseDeDatos/Scripts/Script.Actividad05.sql
@@ -0,0 +1,69 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+-- Actividad N° 05 - Funciones
+	
+
+/* 1. Se requiere realizar una consulta que visualice la fecha del sistema.*/
+	SELECT CONVERT (DATE, SYSDATETIME())
+    ,CONVERT (DATE, SYSDATETIMEOFFSET())
+    ,CONVERT (DATE, SYSUTCDATETIME())
+    ,CONVERT (DATE, CURRENT_TIMESTAMP)
+    ,CONVERT (DATE, GETDATE())
+    ,CONVERT (DATE, GETUTCDATE());
+
+/* 2. El departamento de Recursos Humanos necesita un reporte de todos los empleados que muestre el N°
+de Empleado, Apellidos, Salario y una columna más con el cálculo del salario incrementado en 15.5%
+(expresado solo en enteros) esta columna debe etiquetarse Nuevo Salario.*/
+	SELECT employee_id,last_name,salary,salary*0.155 AS newsalary FROM employees
+
+/* 3. Modificar la consulta anterior y adicionar una columna que muestre el resultado de la resta entre el
+antiguo salario y el nuevo salario. Etiquetar esta columna como Incremento.*/
+	SELECT employee_id,last_name,salary,salary*0.155 AS newsalary,salary-(salary*0.155) AS incremento FROM employees
+
+/* 4. Crear un reporte que muestre los Apellidos (con la primera letra en Mayúsculas y las demás en
+Minúsculas) y la longitud de los apellidos (colocar alias Longitud), para todos aquellos empleados
+quienes sus apellidos empiecen con las letras ‘J’, ‘A’ y ‘M’. Ordenar los resultados por la columna
+Apellido.*/
+	SELECT UPPER(LEFT(last_name,1))+ (LOWER(SUBSTRING(last_name,1,LEN(last_name)))) AS ALEAS FROM employees WHERE last_name LIKE 'J%' OR last_name LIKE 'M%' OR last_name LIKE 'A%' ORDER BY last_name ASC;
+ 
+	SELECT UPPER(last_name) "Apellido", (LOWER(first_name)) "Longitud" 
+	FROM employees 
+	WHERE last_name like 'A%'
+      or last_name like 'J%'
+      or last_name like 'M%' order by last_name asc;
+
+/* 5. Modificar la consulta anterior a fin de que consulte primero al usuario con que letra empieza el apellido
+a buscar. Considerar que no importa si la letra esta mayúscula o minúscula de igual manera debe
+mostrar los resultados.*/
+	SELECT initcap(FIRSST_NAME) AS "name", length(first_name) AS "Length" FROM employees WHERE UPPER(substr(first_name,1,1))=UPPER('&Inicial') ORDER BY first_name; 
+
+/* 6. El departamento de Recursos Humanos la duración o tiempo de permanencia de cada empleado,
+mostrar el Apellido y el calculo del número de meses entre la fecha de hoy y la fecha en que fue
+contratado el empleado, Etiquetar la columna como Meses Trabajados, ordenar los resultados por el
+resultado de los números de meses, Redondear el número de meses al entero más cercano.*/
+	SELECT LAST_NAME, ROUND(MONTH_BETWEEN(SYSDATE,HIRE_DATE),0) "MONTHS_WORK" FROM employees ORDER BY MONTHS_BETWEEN (HIRE_DATE,SYSDATE);
+
+/* 7. Crear una consulta que devuelva los Apellidos y Salarios de todos los empleados, Formatear la columna
+salario para que muestre 15 caracteres, completar con el símbolo ‘$’ los espacios previos al valor de la
+columna salario, ejemplo: $$$$$$$$$$10000. Etiquetar esta columna como Salario.*/
+	SELECT last_name, lpad(salary,15,'$') as salary from employees;
+
+/* 8. Crear una consulta que muestre en una única columna los primeros 8 caracteres del apellido de los
+empleados e indique sus salarios representados por asteriscos (‘*’), cada asterisco representa el valor
+1000. Ordenar el listado por el salario de los empleados. Asimismo Etiquetar la columna como
+‘Empleados y sus Salarios’*/
+	SELECT substr(last_name,1,8) || rpad(' ', salary/1000, '*') AS "EMPLOYEES_AND_THEIR_SALARIES" FROM employees ORDER BY salary;
+
+/* 9. Finalmente crear una consulta que muestre los Apellidos de los empleados y el N° de Semanas
+Empleado hasta la actualidad para todos los empleados del departamento N° 90, truncar el número de
+semanas a sin decimales. Ordenar el resultado por el N° de Semanas y etiquetar la columna tendencia.*/
+	SELECT last_name, TRUNC(((SYSDATE-hire_date)/7),0) as TENURE from employees where department_id=90 ORDER BY hire_date DESC;
---
+++ PrjBaseDeDatos/PrjBaseDeDatos/Scripts/Script.Actividad06.sql
@@ -0,0 +1,53 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+-- Actividad N° 06 -  Funciones de Conversión
+
+/* 1. Crear un reporte que muestre lo siguiente por cada empleado. */
+	SELECT 'Sueldos Soñados'=(last_name + ' gana ' + Cast(salary as varchar(18)) + ' pero quisiera ' + Cast((salary * 3) as varchar(18))) from dbo.employees
+
+/* 2. Realizar una consulta que muestre el Apellido del empleado, fecha de contratación y la Fecha de
+Revisión del Salario, la cual es el primer Lunes después de cada seis meses de servicio, etiquetar la
+columna como Revisión. */
+	SELECT last_name, hire_date as Revision from employees 
+	WHERE hire_date between '2003-06-17' and '2005-09-21' ;
+
+	SELECT last_name, hire_date  FROM employees 
+	WHERE  hire_date LIKE ('200%-06-*') ;
+
+/* 3. Mostrar un reporte que tenga los Apellidos, Fecha de Contratación y el Día de Inicio de cada empleado
+(Lunes, Martes, etc…), etiquetar la última columna como Día. Ordenar los resultados por el Día de Inicio
+empezando por Lunes.*/
+	SELECT e.last_name, e.hire_date, DateName(WEEKDAY, jh.START_DATE)as 'Dia' 
+	FROM dbo.employees as e inner join dbo.job_history as jh on e.employee_id=jh.employee_id
+    
+/* 4. Crear un listado que muestre los Apellidos de los empleados y sus Montos de Comisión, en caso no
+tenga comisión deberá mostrar el texto ‘Sin Comisión’, etiquetar esta ultima columna como Comisión.*/
+
+	--muestras texto comision, si no tiene monto
+	SELECT last_name AS 'Apellidos', 'Comision'='Sin Comision' FROM dbo.employees WHERE commission_pct <= 0 
+	UNION
+	--muestra comision y el calculo de dicha
+	SELECT last_name as 'Apellidos', 'Comision'= Cast((salary * commission_pct) as varchar(20)) from dbo.employees where commission_pct > 0
+
+/* 5. Utilizando la función DECODE, crear un reporte que muestre los apellidos, los puestos y los grados de
+los empleados basados en sus puestos.*/
+
+/* 6. Rescribir la consulta anterior utilizando la función CASE.
+Guardar los comandos y las respuestas en el archivo lab_04_06.sql*/
+	SELECT e.last_name as 'Apellidos', j.job_title, case 
+	when j.job_id = 'AD_PRES' THEN 'A'
+	WHEN j.job_id = 'ST_MAN' THEN 'B'
+	WHEN j.job_id = 'IT_PROG' THEN 'C'
+	WHEN j.job_id = 'SA_REP' THEN 'D'
+	WHEN j.job_id = 'ST_CLERK' THEN 'E'
+	ELSE '0' END AS 'Grados' FROM dbo.employees AS e INNER JOIN dbo.jobs AS j ON e.job_id=j.job_id
+	GO
